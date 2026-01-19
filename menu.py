fcb2c474d407c55edd36d80ff061f17bef34d3c2
nelson-grac-aubert <nelson.grac-aubert@laplateforme.io>
2026-01-19 13:31:50 +0100

    feat : set main menu background

--- menu.py
+++ menu.py
@@ -1,8 +1,12 @@
 import pygame 
 
+pygame.init()
 (width, height) = (1000, 600)
 clock = pygame.time.Clock()
 screen = pygame.display.set_mode((width, height))
+
+blackboard = pygame.image.load("blackboard.png")
+blackboard = pygame.transform.scale(blackboard, (1000, 600))
 
 pygame.display.flip()
 pygame.display.set_caption('Pendu')
@@ -11,9 +15,11 @@ running = True
 main_menu = True 
 
 while running:
+    screen.fill("white")
+    screen.blit(blackboard,(0,0))
     clock.tick(30)
+    pygame.display.flip()
+    
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-
-    # while main_menu == True : 
