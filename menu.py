648e280a1605d98658f3ca3e56074e0880fd7704
nelson-grac-aubert <nelson.grac-aubert@laplateforme.io>
2026-01-19 13:48:08 +0100

    feat : add fonts, icons, game title

--- menu.py
+++ menu.py
@@ -5,8 +5,12 @@ pygame.init()
 clock = pygame.time.Clock()
 screen = pygame.display.set_mode((width, height))
 
-blackboard = pygame.image.load("blackboard.png")
+blackboard = pygame.image.load("assets/blackboard.png")
 blackboard = pygame.transform.scale(blackboard, (1000, 600))
+
+chalk_font = pygame.font.Font('assets/fonts/FrederickatheGreat-Regular.ttf', 45)
+
+title_surface = chalk_font.render('Hangman X _ X', False, (255, 255, 255))
 
 pygame.display.flip()
 pygame.display.set_caption('Pendu')
@@ -17,6 +21,7 @@ main_menu = True
 while running:
     screen.fill("white")
     screen.blit(blackboard,(0,0))
+    screen.blit(title_surface, (305,120))
     clock.tick(30)
     pygame.display.flip()
     
